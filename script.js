1578d0854bfe0545a59ff05781099b213ac952e2
albjoh2 <aljo940616@gmail.com>
2022-05-01 14:24:53 +0200

    added netlify form function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const form = document.querySelector('.inputcontainer');
+
+function handleForm(event) {
+  event.preventDefault();
+  setTimeout('form.submit()', 3000);
+}
+
+form.addEventListener('submit', handleForm);
